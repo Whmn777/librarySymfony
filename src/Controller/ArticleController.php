a7edaf51db4d82865bd229ecb7e65a20c2e3068e
Whmn777 <helene.hnawia@lapiscine.pro>
2020-11-25 12:17:34 +0100

    class EntityManager - méthodes persist et flush insert-static

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Article;
 
 class ArticleController extends AbstractController
 
@@ -41,7 +42,7 @@ class ArticleController extends AbstractController
 
     //Je crée une nouvelle route pour accéder à la page de chaque article, en utilisant la wilde card {id}
     /**
-     * @Route("/article/{id}", name="article_show")
+     * @Route("/article/show/{id}", name="article_show")
      */
 
     //Je crée une méthode publique articleShow ayant pour paramètres, la valeur de la wilde card {id}
@@ -70,5 +71,49 @@ class ArticleController extends AbstractController
 
     }
 
+    /**
+     * Je créee un nouvelle route pour insérer des données statiques (i.e) stockées sur mon Controller.
+     *
+     * @Route("/article/insert-static", name="article_insert_static")
+     */
 
+
+    //Je créee une méthode publique inserStaticArticle ayant en paramètre la classe EntityManagerInterface qui
+    // va me permettre de réaliser des requêtes INSERT, UPDATE et DELETE dans ma BDD. Je rajoute la variable
+    //$entityManager
+
+    public function insertStaticArticle(EntityManagerInterface $entityManager )
+    {
+
+        //Dans la variable $article, j'instancie un nouvel objet newArticle de la classe entité Article.
+        //Je peux ainsi redéfinir à ma class entité des nouvelles propriétés, qui seront insérer comme des
+        //nouveaux champs sur ma table article dans ma BDD.
+
+       $article = new Article();
+
+       //J'utilise des setteurs pour modifier les proriétés de ma class entité Article.
+
+        $article->setTitle("Insérer des Données Statiques");
+        $article->setContent("J'utilise ma fonction insertArticle, avec pour paramètres la class EntityManagerInterface
+          et la variable entityManager");
+        $article->setImage("https://secure.meetupstatic.com/photos/event/1/3/c/1/600_467885057.jpeg");
+        $article->setCreationDate(new \DateTime());
+        $article->setPublicationDate(new \DateTime());
+        $article->setIsPublished(true);
+
+        //A ma variable $entityManager, je pré-sauvegarde mes nouvelles données en utilisant la méthode persist
+        //de la class EntityManagerInterface) (équivalent du git commit dans Git)
+
+        $entityManager->persist($article);
+
+        //J'insère mes données statiques dans ma table article de ma BDD,
+        //en utilisant la méthode flush de la class EntityManagerInterface
+
+        $entityManager->flush();
+
+        //
+
+        return $this->render('insert_static.html.twig');
+
+    }
 }
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Category;
 use Symfony\Component\Routing\Annotation\Route;
@@ -37,6 +38,51 @@ class CategoryController extends AbstractController
         return $this->render('category.html.twig', [
             'categories' => $categories
         ]);
+
+    }
+
+    /**
+     * Je créee un nouvelle route pour insérer des données statiques (i.e) stockées sur mon Controller.
+     *
+     * @Route("/category/insert-static", name="category_insert_static")
+     */
+
+
+    //Je créee une méthode publique inserStaticCategory ayant en paramètre la classe EntityManagerInterface qui
+    // va me permettre de réaliser des requêtes INSERT, UPDATE et DELETE dans ma BDD. Je rajoute la variable
+    //$entityManager
+
+    public function insertStaticCategory(EntityManagerInterface $entityManagerInterface )
+    {
+
+        //Dans la variable $categorie, j'instancie un nouvel objet newCategory de la classe entité Category.
+        //Je peux ainsi redéfinir à ma class entité des nouvelles propriétés, qui seront insérer comme des
+        //nouveaux champs sur ma table categorie dans ma BDD.
+
+        $categorie = new Category();
+
+        //J'utilise des setteurs pour modifier les proriétés de ma class entité Category.
+
+        $categorie->setTitle("Langage de programmation");
+        $categorie->setColor("J'utilise ma fonction insertArticle, avec pour paramètres la class EntityManagerInterface
+          et la variable entityManager");
+        $categorie->setCreationDate(new \DateTime());
+        $categorie->setPublicationDate(new \DateTime());
+        $categorie->setIsPublished(true);
+
+        //A ma variable $entityManager, je pré-sauvegarde mes nouvelles données en utilisant la méthode persist
+        //de la class EntityManagerInterface) (équivalent du git commit dans Git)
+
+        $entityManagerInterface->persist($categorie);
+
+        //J'insère mes données statiques dans ma table catégorie de ma BDD,
+        //en utilisant la méthode flush de la class EntityManagerInterface
+
+        $entityManagerInterface->flush();
+
+        //
+
+        return $this->render('category_insert_static.html.twig');
 
     }
 }
